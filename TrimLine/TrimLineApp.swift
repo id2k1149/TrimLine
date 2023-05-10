a118b4d0bebbc1978805ee20974de6fac448476a
id2k1149 <id2k1149@gmail.com>
2023-05-10 11:12:41 -0400

    Initial Commit

---
+++ TrimLine/TrimLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrimLineApp.swift
+//  TrimLine
+//
+//  Created by Max Franz Immelmann on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TrimLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
